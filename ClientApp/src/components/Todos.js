152e3c637b88f88bb64289ff1c9d621bcb3d8fbc
theschmocker <jacobcs1996@gmail.com>
2020-03-25 18:34:40 -0500

    make todo text editable

--- ClientApp/src/components/Todos.js
+++ ClientApp/src/components/Todos.js
@@ -45,7 +45,45 @@ const useTodos = () => {
     }
 }
 
-function TodoList({ loading, todos, onToggle, onDelete }) {
+function Todo ({ todo, onToggle, onDelete, onUpdate }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const onSave = todo => onUpdate(todo);
+
+    if (isEditing) return <TodoEditor todo={todo} onCancel={() => setIsEditing(false)} onSave={onSave}/>
+
+    return (
+        <li className="list-group-item d-flex align-items-center">
+            <input type="checkbox" checked={todo.done} onChange={() => onToggle(todo)} />
+            <span className="ml-2" style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>{todo.text}</span>
+            <button className="btn btn-outline-secondary ml-auto" onClick={() => setIsEditing(true)}>Edit</button>
+            <button className="btn btn-outline-secondary ml-1" onClick={() => onDelete(todo)}>Delete</button>
+        </li>
+    );
+}
+
+function TodoEditor ({ todo, onCancel, onSave }) {
+    const [text, setText] = useState(todo.text);
+
+    const save = () => { 
+        onSave({ ...todo, text });
+        onCancel();
+    };
+
+    return (
+        <li className="list-group-item d-flex align-items-center">
+            <div className="form-group mb-0">
+                <input className="form-control" value={text} onChange={e => setText(e.target.value)} />
+            </div>
+            <div className="form-group mb-0 ml-auto">
+                <button className="btn btn-outline-secondary" onClick={onCancel}>Cancel</button>
+                <button className="btn btn-outline-secondary ml-1" onClick={save}>Save</button>
+            </div>
+        </li>
+    );
+}
+
+function TodoList({ loading, todos, onToggle, onDelete, onUpdate }) {
     if (loading) {
         return (
             <div className="spinner-grow text-primary" role="status">
@@ -57,11 +95,15 @@ function TodoList({ loading, todos, onToggle, onDelete }) {
     return(
         todos && todos.length ? (
             <ul className="list-group">
-                    {todos.map(todo => ( <li className="list-group-item d-flex align-items-center" key={todo.id}>
-                        <input type="checkbox" checked={todo.done} onChange={() => onToggle(todo)} />
-                        <span className="ml-2" style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>{todo.text}</span>
-                        <button className="btn btn-outline-secondary ml-auto" onClick={() => onDelete(todo)}>Delete</button>
-                    </li>))}
+                    {todos.map(todo => ( 
+                        <Todo 
+                            todo={todo}
+                            onToggle={onToggle}
+                            onDelete={onDelete}
+                            onUpdate={onUpdate}
+                            key={todo.id}
+                        />
+                    ))}
             </ul>
         ) : (
             <div>No todos. Have a great day!</div>
@@ -102,6 +144,7 @@ function Todos() {
                 todos={todos}
                 onToggle={todo => toggleDone(todo)}
                 onDelete={todo => deleteTodo(todo)}
+                onUpdate={updateTodo}
             />
         </div>
     );
